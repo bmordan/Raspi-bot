5872f2496575ee6e444b941f92f26ecfce2a9166
bmordan <bernardmordan@gmail.com>
2015-03-09 22:03:13 +0000

    updated to raspi.io 3.0.0

--- init.js
+++ init.js
@@ -3,36 +3,79 @@ var raspi = require('raspi-io')
 var board = new five.Board({
   io: new raspi()
 })
+var dgram = require('dgram')
+var listen = dgram.createSocket('udp4')
+listen.bind(3000)
 
 board.on('ready', function () {
 
+  this.pinMode('GPIO18', five.Pin.PMW)
   this.pinMode('GPIO17', five.Pin.OUTPUT)
+
   this.pinMode('GPIO6', five.Pin.OUTPUT)
   var l = new five.Motor(['GPIO18','GPIO17'])
   var r = new five.Motor(['GPIO12','GPIO6'])
 
-  var m = ({
-    f: function () {
-      l.forward(100)
-      r.forward(100)
-    },
-    r: function () {
-      l.reverse(100)
-      r.reverse(100)
-    },
-    s: function () {
-      l.stop()
-      r.stop()
+  listen.on('message', function (msg, rinfo) {
+    var payload = JSON.parse(msg)
+    switch (payload.motor+payload.dir) {
+      case 'leftforward':
+        motor.leftforward()
+        break
+      case 'leftreverse':
+        motor.leftreverse()
+        break
+      case 'leftstop':
+        motor.leftstop()
+        break
+      case 'rightforward':
+        motor.rightforward()
+        break
+      case 'rightreverse':
+        motor.rightreverse()
+        break
+      case 'rightstop':
+        motor.rightstop()
+        break
     }
   })
 
-  board.wait(200, function () {
-    m.s()
+  board.wait(300, function () {
+    motor.reverse()
+    motor.stop()
   })
-  m.r()
 
   board.repl.inject({
-    m: m
+    r: r
+  })
+
+  var motor = ({
+    stop: function () {
+      l.stop()
+      r.stop()
+    },
+    leftstop: function () {
+      l.stop()
+    },
+    rightstop: function () {
+      r.stop()
+    },
+    leftforward: function () {
+      l.forward(60)
+    },
+    rightforward: function () {
+      r.forward(60)
+    },
+    leftreverse: function () {
+      l.reverse(160)
+    },
+    rightreverse: function () {
+      r.reverse(160)
+    },
+    reverse: function () {
+      l.reverse(160)
+      r.reverse(160)
+    }
   })
 
 })
